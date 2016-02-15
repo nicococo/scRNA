fe8cf4ca0783ba6e8bf76e71f40d3690f266fbc5
nicococo <sinnl0s>
2016-02-15 15:54:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### Description
+Not sure yet :)
---
+++ setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'scRNA - scRNA-seq clustering toolbox',
+    'url': 'https://github.com/nicococo/scRNA',
+    'author': 'Nico Goernitz',
+    'author_email': 'nico.goernitz@tu-berlin.de',
+    'version': '0.1',
+    'install_requires': ['nose', 'cvxopt','scikit-learn'],
+    'packages': ['scRNA'],
+    'scripts': [],
+    'name': 'scRNA',
+    'classifiers':['Intended Audience :: Science/Research',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering',
+                   'Operating System :: POSIX',
+                   'Operating System :: Unix',
+                   'Operating System :: MacOS',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7']
+}
+
+setup(**config)
