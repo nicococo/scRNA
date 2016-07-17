89d5dd0abe57942fcf03dcc2c29f6e5505cdcad0
nicococo <sinnl0s>
2016-07-17 15:04:42 +0200

    Towards command line usable pipeline

--- scRNA/cmd_test.py
+++ scRNA/cmd_test.py
@@ -1,8 +1,6 @@
 import argparse, sys
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    arguments = parser.parse_args(sys.argv[1:])
-    print('Parameters:')
-    print arguments
-    print
+parser = argparse.ArgumentParser()
+arguments = parser.parse_args(sys.argv[1:])
+print('Parameters:')
+print arguments
