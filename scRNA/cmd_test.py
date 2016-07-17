8b3338d652d28cdc802ca9e0ba7ff1f7e96e80da
nicococo <sinnl0s>
2016-07-17 15:10:14 +0200

    Towards command line usable pipeline

--- scRNA/cmd_test.py
+++ scRNA/cmd_test.py
@@ -1,7 +1,11 @@
-import argparse
+import argparse, sys
 
-def main(args):
-    parser = argparse.ArgumentParser()
-    arguments = parser.parse_args(args)
-    print('Parameters:')
-    print arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-d","--dataset", help="Dataset to run (default Toy)", default="Toy", type=str)
+parser.add_argument("-o","--hold_out", help="Fraction of hold-out examples for reps (default 0.0)", default=0.0, type =float)
+parser.add_argument("-r","--reps", help="number repetitions (default 1)", default=1, type =int)
+parser.add_argument("-e","--experiment", help="active experiment [0-2] (default 1)", default=1, type =int)
+
+arguments = parser.parse_args(sys.argv[1:])
+print('Parameters:')
+print arguments
