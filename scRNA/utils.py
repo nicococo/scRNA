d7d774d1c1fa750bf2f30e22241c6f8f11546015
bettinanana <bettina.mieth@tu-berlin.de>
2016-07-19 10:58:16 +0200

    updated utils

--- scRNA/utils.py
+++ scRNA/utils.py
@@ -73,9 +73,11 @@ def kta_align_binary(K, y):
     return K.dot(YY).trace() / (m * np.sqrt(K.dot(K.T).trace()))
 
 
-def generate_toy_data(num_genes=5000, num_cells= 400,num_clusters= 5, dirichlet_parameter_cluster_size=1,
-                      shape_power_law=0.1, upper_bound_counts=300000, dirichlet_parameter_counts=1):
+def generate_toy_data(num_genes=5000, num_cells=400, num_clusters=5, dirichlet_parameter_cluster_size=1, mode=1, shape_power_law=0.1, upper_bound_counts=300000,
+                          dirichlet_parameter_counts=1, binomial_parameter=1e-05):
+
     # Toy experiment parameters
+    # mode: How are total counts generated? 1 = Power law, 2 = Negative Binomial Distribution
     # shape_power_law = 0.1  # shape parameter of the power law -  between 0 and 1, the smaller this value the more extreme the power law
     # num_clusters = 5
     # dirichlet_parameter_cluster_size = 1  # between 0 and inf, smaller values make cluster sizes more similar
@@ -99,10 +101,20 @@ def generate_toy_data(num_genes=5000, num_cells= 400,num_clusters= 5, dirichlet_
     labels_now = []
     for cluster_ind in range(num_clusters):
         # Draw samples from the power law distribution
-        sample = np.round(np.random.power(shape_power_law, num_genes) * upper_bound_counts)
+
+        # pdb.set_trace()
+        if mode == 1:
+            sample = np.round(np.random.power(shape_power_law, num_genes) * upper_bound_counts)
+        elif mode == 2:
+            sample = np.random.negative_binomial(1, binomial_parameter, num_genes)
+        else:
+            print "Wrong mode!"
 
         # the abundance plot
         # plt.plot(np.sort(sample)[::-1], 'o')
+        # plt.plot(np.sort(sample_nb)[::-1], 'o')
+        # plt.show()
+
 
         # Generate data
         data_now = []
@@ -111,11 +123,92 @@ def generate_toy_data(num_genes=5000, num_cells= 400,num_clusters= 5, dirichlet_
             one_gene = np.round(np.random.dirichlet(np.ones(cluster_sizes[cluster_ind]) * dirichlet_parameter_counts, size=1) * row_total)
             data_now.append(one_gene)
 
-        # plt.show()
         data_complete.append(np.squeeze(np.asarray(data_now)))
-        labels_now.append(np.tile(cluster_ind+1, cluster_sizes[cluster_ind]))
+        labels_now.append(np.tile(cluster_ind + 1, cluster_sizes[cluster_ind]))
 
     assert min(cluster_sizes) > 1
     data = np.squeeze(np.hstack(data_complete))
     labels = np.hstack(labels_now)
     return [data, labels]
+
+
+def split_source_target(toy_data, true_toy_labels, proportion_target, mode):
+    # 1 = split randomly, 2 = split randomly, but stratified, 3 = Have some overlapping and some exclusive clusters, 4 = have only exclusive clusters
+
+    if mode == 1:
+        toy_data_source, toy_data_target, true_toy_labels_source, true_toy_labels_target = train_test_split(np.transpose(toy_data), true_toy_labels,
+                                                                                                            test_size=proportion_target)
+    elif mode == 2:
+        toy_data_source, toy_data_target, true_toy_labels_source, true_toy_labels_target = train_test_split(np.transpose(toy_data), true_toy_labels,
+                                                                                                            test_size=proportion_target, stratify=true_toy_labels)
+    elif mode == 3:
+        cluster_names = np.unique(true_toy_labels)
+
+        # Assign exclusive clusters
+        # To source data set
+        source_cluster = cluster_names[0]
+        source_indices = (true_toy_labels == source_cluster)
+        toy_data_source_exclusive = toy_data[:, source_indices]
+        true_toy_labels_source_exclusive = true_toy_labels[source_indices]
+
+        # To target data set
+        target_cluster = cluster_names[1]
+        target_indices = (true_toy_labels == target_cluster)
+        toy_data_target_exclusive = toy_data[:, target_indices]
+        true_toy_labels_target_exclusive = true_toy_labels[target_indices]
+
+        # Distribute the rest randomly
+        random_clusters = cluster_names[2:]
+        random_indices_mat = []
+        for i in range(len(random_clusters)):
+            random_indices_mat.append(true_toy_labels == random_clusters[i])
+        random_indices = np.any(random_indices_mat, 0)
+        toy_data_random = toy_data[:, random_indices]
+        true_toy_labels_random = true_toy_labels[random_indices]
+
+        toy_data_source_random, toy_data_target_random, true_toy_labels_source_random, true_toy_labels_target_random = train_test_split(np.transpose(toy_data_random),
+                                                                                                                 true_toy_labels_random, test_size=proportion_target)
+
+        # Combine exclusive and random data
+        toy_data_source = np.concatenate((toy_data_source_exclusive, np.transpose(toy_data_source_random)), axis=1)
+        toy_data_target = np.concatenate((toy_data_target_exclusive, np.transpose(toy_data_target_random)), axis=1)
+        true_toy_labels_source = np.concatenate((true_toy_labels_source_exclusive, np.transpose(true_toy_labels_source_random)))
+        true_toy_labels_target = np.concatenate((true_toy_labels_target_exclusive, np.transpose(true_toy_labels_target_random)))
+
+        # toy_data_rebuilt = np.concatenate((toy_data_source, toy_data_target), axis=1)
+        # toy_labels_rebuilt = np.concatenate((true_toy_labels_source, true_toy_labels_target), axis=0)
+
+        # TSNE_plot(toy_data_rebuilt, toy_labels_rebuilt)
+        # TSNE_plot(toy_data, true_toy_labels)
+        # TSNE_plot(toy_data_source, true_toy_labels_source)
+        # TSNE_plot(toy_data_target, true_toy_labels_target)
+
+    elif mode == 4:
+
+        cluster_names = np.unique(true_toy_labels)
+        source_clusters = cluster_names[0: int(len(cluster_names) * proportion_source)]
+        target_clusters = cluster_names[int(len(cluster_names) * proportion_source):len(cluster_names)]
+
+        source_indices_mat = []
+        for i in range(len(source_clusters)):
+            source_indices_mat.append(true_toy_labels == source_clusters[i])
+        source_indices = np.any(source_indices_mat, 0)
+        toy_data_source = toy_data[:, source_indices]
+        true_toy_labels_source = true_toy_labels[source_indices]
+
+        target_indices_mat = []
+        for i in range(len(target_clusters)):
+            target_indices_mat.append(true_toy_labels == target_clusters[i])
+        target_indices = np.any(target_indices_mat,0)
+        toy_data_target = toy_data[:,target_indices]
+        true_toy_labels_target = true_toy_labels[target_indices]
+
+    else:
+        print "Unknown mode!"
+        toy_data_source=[]
+        toy_data_target=[]
+        true_toy_labels_source=[]
+        true_toy_labels_target = []
+
+    return toy_data_source, toy_data_target, true_toy_labels_source, true_toy_labels_target
+
