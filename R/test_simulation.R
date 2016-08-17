f1c02c2dba9713fca097e997dcbbcdd5632b262b
Alex Gutteridge <alex.gutteridge@pfizer.com>
2016-08-17 14:40:29 +0100

    changes to the R test code

--- R/test_simulation.R
+++ R/test_simulation.R
@@ -1,8 +1,8 @@
 library(readr)
 library(pheatmap)
 
-source.fname = "source_data_T1_100_S1_1000.tsv"
-target.fname = "target_data_T1_100_S1_1000.tsv"
+source.fname = "source_data_T5_500_S5_500.tsv"
+target.fname = "target_data_T5_500_S5_500.tsv"
 
 source.data = read_tsv(source.fname, col_names = FALSE)
 target.data = read_tsv(target.fname, col_names = FALSE)
@@ -10,9 +10,9 @@ target.data = read_tsv(target.fname, col_names = FALSE)
 colnames(source.data) = as.character(1:ncol(source.data))
 colnames(target.data) = as.character((ncol(source.data)+1):(ncol(source.data)+ncol(target.data)))
 
-source.labels = read_tsv("source_labels_T1_100_S1_1000.tsv", col_names = FALSE)
+source.labels = read_tsv("source_labels_T5_500_S5_500.tsv", col_names = FALSE)
 source.labels$X1 = factor(source.labels$X1)
-target.labels = read_tsv("target_labels_T1_100_S1_1000.tsv", col_names = FALSE)
+target.labels = read_tsv("target_labels_T5_500_S5_500.tsv", col_names = FALSE)
 target.labels$X1 = factor(target.labels$X1)
 
 source.dist.mat = cor(source.data)
